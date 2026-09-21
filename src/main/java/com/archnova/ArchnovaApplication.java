10493e7522fa8255b923ad9fe1e68d04c5a3c292
lizhenghang <1135365441@qq.com>
2026-09-21 10:42:16 +0800

    第一版

--- src/main/java/com/archnova/ArchnovaApplication.java
+++ src/main/java/com/archnova/ArchnovaApplication.java
@@ -3,8 +3,9 @@ package com.archnova;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @MapperScan("com.archnova.mapper")
 public class ArchnovaApplication {
 
